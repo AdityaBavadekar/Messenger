c3d5b25bfbd66a7ad732295c714c1857035cb785
Aditya Bavadekar <64344960+AdityaBavadekar@users.noreply.github.com>
2022-10-22 23:19:08 +0530

    Bump version to 0.0.5

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -17,27 +17,10 @@
  */
 
 @file:Suppress("unused")
-/*
-*
-*    Copyright 2022 Aditya Bavadekar
-*
-*    Licensed under the Apache License, Version 2.0 (the "License");
-*    you may not use this file except in compliance with the License.
-*    You may obtain a copy of the License at
-*
-*        http://www.apache.org/licenses/LICENSE-2.0
-*
-*    Unless required by applicable law or agreed to in writing, software
-*    distributed under the License is distributed on an "AS IS" BASIS,
-*    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*    See the License for the specific language governing permissions and
-*    limitations under the License.
-*
-*/
 
 object Versions {
 
-    const val VERSION_NAME = "0.0.4" //XX.YY.ZZ [X:Major][Y:Minor][Z:Patch]
+    const val VERSION_NAME = "0.0.5" //XX.YY.ZZ [X:Major][Y:Minor][Z:Patch]
     val VERSION_CODE = versionCodeGet()
 
     const val COMPILE_SDK = 31
