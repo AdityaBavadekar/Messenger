ec24134f1a51f1d55da2b0aba3f7b7b875ba9b04
Aditya Bavadekar <bvkraditya@gmail.com>
2023-02-15 16:15:48 +0530

    Final commit for 0.0.5

---
+++ app/src/main/java/com/adityaamolbavadekar/messenger/utils/AppCheckUtil.kt
@@ -0,0 +1,15 @@
+package com.adityaamolbavadekar.messenger.utils
+
+import android.content.Context
+
+class AppCheckUtil {
+    companion object {
+        fun activate(context: Context) {
+            /*FirebaseApp.initializeApp(context)
+            val firebaseAppCheck = FirebaseAppCheck.getInstance()
+            firebaseAppCheck.installAppCheckProviderFactory(
+                PlayIntegrityAppCheckProviderFactory.getInstance()
+            )*/
+        }
+    }
+}
